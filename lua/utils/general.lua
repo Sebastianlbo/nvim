0e2325671a15e3f76d43fb83ad0b10ba5439cee0
Seb <sebastianlbo@icloud.com>
2026-02-18 14:49:15 +0100

    feat: add util to properly get latex files in path

--- lua/utils/general.lua
+++ lua/utils/general.lua
@@ -9,9 +9,20 @@ end
 
 function M.setup_mason_path()
   local mason_bin = fn.stdpath("data") .. "/mason/bin"
+  local tex_bins = {
+    "/Library/TeX/texbin",
+    "/usr/local/texlive/2025/bin/universal-darwin",
+  }
 
   if fn.isdirectory(mason_bin) == 1 and not string.find(vim.env.PATH, mason_bin, 1, true) then
     vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
+  end
+
+  for _, tex_bin in ipairs(tex_bins) do
+    if fn.isdirectory(tex_bin) == 1 and not string.find(vim.env.PATH, tex_bin, 1, true) then
+      vim.env.PATH = tex_bin .. ":" .. vim.env.PATH
+      break
+    end
   end
 end
 
