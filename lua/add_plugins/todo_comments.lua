479346b8754a3e4640c29c68cebb4cb59edf1fa6
Seb <sebastianlbo@icloud.com>
2026-03-06 01:51:17 +0100

    feat: add custom todos

--- lua/add_plugins/todo_comments.lua
+++ lua/add_plugins/todo_comments.lua
@@ -1,12 +1,25 @@
 return {
-  "folke/todo-comments.nvim",
-  event = "VimEnter",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    signs = true,
-    sign_priority = 2,
-    search = {
-      pattern = [[\#\s*\b(KEYWORDS):]],
-    },
-  },
+	"folke/todo-comments.nvim",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+
+	init = function()
+		require("utils").load_mappings("todo_comments")
+	end,
+	opts = {
+		signs = true,
+		sign_priority = 2,
+		keywords = {
+			BUG = { icon = " ", color = "#FF8C00", alt = { "Bug", "bug" } },
+			REVIEW = { icon = " ", color = "#4FC3F7", alt = { "review", "Review" } },
+			DEEPREVIEW = { icon = " ", color = "#4FC3F7", alt = { "DEEP-REVIEW", "deep-review" } },
+		},
+		highlight = {
+			keyword = "fg",
+			pattern = [[.*\#\s*(KEYWORDS)]],
+		},
+		search = {
+			pattern = [=[(?i)\#\s*(KEYWORDS)]=],
+		},
+	},
 }
