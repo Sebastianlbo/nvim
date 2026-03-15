b60cf04fccfa59e1b92aa7e28b0444e1ef0b9298
Seb <sebastianlbo@icloud.com>
2026-03-15 22:53:00 +0100

    feat: change size of floating tearm

--- lua/core/plugins/nvterm.lua
+++ lua/core/plugins/nvterm.lua
@@ -1,17 +1,28 @@
 return {
-  "zbirenbaum/nvterm",
-  opts = {
-    behavior = {
-      close_on_exit = false,
-    },
-  },
-  init = function()
-    require("utils").load_mappings("nvterm")
-  end,
-  config = function(_, opts)
-    local ok, nvterm = pcall(require, "nvterm")
-    if ok then
-      nvterm.setup(opts or {})
-    end
-  end,
+	"zbirenbaum/nvterm",
+	opts = {
+		behavior = {
+			close_on_exit = false,
+		},
+		terminals = {
+			type_opts = {
+				float = {
+					relative = "editor",
+					row = 0.125,
+					col = 0.125,
+					width = 0.75,
+					height = 0.75,
+				},
+			},
+		},
+	},
+	init = function()
+		require("utils").load_mappings("nvterm")
+	end,
+	config = function(_, opts)
+		local ok, nvterm = pcall(require, "nvterm")
+		if ok then
+			nvterm.setup(opts or {})
+		end
+	end,
 }
